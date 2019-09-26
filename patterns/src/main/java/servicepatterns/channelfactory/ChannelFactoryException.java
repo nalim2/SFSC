faafe3ad499996c86316513cd50913d3fcb9e1ee
Chris Vollmann <st109090@stud.uni-stuttgart.de>
2019-09-26 23:24:17 +0200

    rework of ChannelFactory

---
+++ patterns/src/main/java/servicepatterns/channelfactory/ChannelFactoryException.java
@@ -0,0 +1,20 @@
+package servicepatterns.channelfactory;
+
+public class ChannelFactoryException extends RuntimeException {
+
+  public ChannelFactoryException() {
+    super();
+  }
+
+  public ChannelFactoryException(String message) {
+    super(message);
+  }
+
+  public ChannelFactoryException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public ChannelFactoryException(Throwable cause) {
+    super(cause);
+  }
+}
