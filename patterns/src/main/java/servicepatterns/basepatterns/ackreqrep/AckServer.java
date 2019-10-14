02ba16496412a3d3e068cf57d9bcef6d65f1ce9d
Chris Vollmann <st109090@stud.uni-stuttgart.de>
2019-10-14 16:59:33 +0200

    injection of callback registry to allow closing

--- patterns/src/main/java/servicepatterns/basepatterns/ackreqrep/AckServer.java
+++ patterns/src/main/java/servicepatterns/basepatterns/ackreqrep/AckServer.java
@@ -5,24 +5,27 @@ import de.unistuttgart.isw.sfsc.commonjava.zmq.pubsubsocketpair.PubSubConnection
 import java.util.concurrent.Executor;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.function.Function;
+import servicepatterns.CallbackRegistry;
 import servicepatterns.topiclistener.HandleFactory;
 import servicepatterns.topiclistener.ListenerHandle;
 
 public final class AckServer implements AutoCloseable {
 
+  private final CallbackRegistry callbackRegistry = new CallbackRegistry();
   private final ListenerHandle listenerHandle;
 
   public AckServer(OutputPublisher publisher, ScheduledExecutorService scheduledExecutorService, Function<ByteString, AckServerResult> serverFunction,
       ByteString serverTopic, int timeoutMs, int sendRateMs, int sendMaxTries, HandleFactory handleFactory, Executor executor) {
     listenerHandle = handleFactory.attach(
         serverTopic,
-        new AckServerConsumer(publisher, scheduledExecutorService, serverFunction, serverTopic, timeoutMs, sendRateMs, sendMaxTries),
+        new AckServerConsumer(publisher, callbackRegistry, scheduledExecutorService, serverFunction, serverTopic, timeoutMs, sendRateMs, sendMaxTries),
         executor);
   }
 
   @Override
   public void close() {
     listenerHandle.close();
+    callbackRegistry.close();
   }
 }
 
