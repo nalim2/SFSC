999bba46645dcd95742ed7f4bd6d1a6411741aae
chrizzly123 <24834128+chrizzly123@users.noreply.github.com>
2020-05-27 19:01:02 +0200

    make RegexDefinition optional

--- framework/src/main/java/de/unistuttgart/isw/sfsc/framework/api/services/clientserver/SfscServerImplementation.java
+++ framework/src/main/java/de/unistuttgart/isw/sfsc/framework/api/services/clientserver/SfscServerImplementation.java
@@ -6,6 +6,7 @@ import de.unistuttgart.isw.sfsc.commonjava.zmq.pubsubsocketpair.PubSubConnection
 import de.unistuttgart.isw.sfsc.framework.api.services.ServiceFactory;
 import de.unistuttgart.isw.sfsc.framework.descriptor.SfscServiceDescriptor;
 import de.unistuttgart.isw.sfsc.framework.descriptor.SfscServiceDescriptor.ServerTags;
+import de.unistuttgart.isw.sfsc.framework.descriptor.SfscServiceDescriptor.ServerTags.RegexDefinition;
 import de.unistuttgart.isw.sfsc.framework.patterns.ackreqrep.AckServer;
 import de.unistuttgart.isw.sfsc.framework.patterns.ackreqrep.AckServerResult;
 import java.util.Optional;
@@ -14,31 +15,40 @@ import java.util.function.Function;
 public final class SfscServerImplementation implements SfscServer {
 
   private static final int defaultSendMaxTries = 3;
+  private static final RegexDefinition defaultRegex = RegexDefinition.getDefaultInstance();
 
   private final SfscServiceDescriptor descriptor;
   private final Runnable closeCallback;
 
-  public SfscServerImplementation(SfscServerParameter parameter, ServiceFactory serviceFactory, Function<ByteString, AckServerResult> serverFunction) {
+  public SfscServerImplementation(SfscServerParameter parameter, ServiceFactory serviceFactory,
+      Function<ByteString, AckServerResult> serverFunction) {
     PubSubConnection pubSubConnection = serviceFactory.pubSubConnection();
     String serviceId = serviceFactory.createServiceId();
-    descriptor = SfscServiceDescriptor.newBuilder()
+    descriptor = SfscServiceDescriptor
+        .newBuilder()
         .setServiceId(serviceId)
         .setAdapterId(serviceFactory.adapterId())
         .setCoreId(serviceFactory.coreId())
         .setServiceName(Optional.ofNullable(parameter.getServiceName()).orElse(serviceId))
         .putAllCustomTags(Optional.ofNullable(parameter.getCustomTags()).orElseGet(serviceFactory::defaultCustomTags))
-        .setServerTags(ServerTags.newBuilder()
+        .setServerTags(ServerTags
+            .newBuilder()
             .setInputTopic(Optional.ofNullable(parameter.getInputTopic()).orElseGet(serviceFactory::createTopic))
-            .setInputMessageType(Optional.ofNullable(parameter.getInputMessageType()).orElseGet(serviceFactory::defaultType))
-            .setOutputMessageType(Optional.ofNullable(parameter.getOutputMessageType()).orElseGet(serviceFactory::defaultType))
-            .setRegex(parameter.getRegexDefinition())
+            .setInputMessageType(Optional
+                .ofNullable(parameter.getInputMessageType())
+                .orElseGet(serviceFactory::defaultType))
+            .setOutputMessageType(Optional
+                .ofNullable(parameter.getOutputMessageType())
+                .orElseGet(serviceFactory::defaultType))
+            .setRegex(Optional.ofNullable(parameter.getRegexDefinition()).orElse(defaultRegex))
             .setTimeoutMs(Optional.ofNullable(parameter.getTimeoutMs()).orElseGet(serviceFactory::defaultTimeoutMs))
             .setSendRateMs(Optional.ofNullable(parameter.getSendRateMs()).orElseGet(serviceFactory::defaultTimeoutMs))
             .setSendMaxTries(Optional.ofNullable(parameter.getSendMaxTries()).orElse(defaultSendMaxTries))
             .build())
         .build();
 
-    AckServer server = new AckServer(pubSubConnection,
+    AckServer server = new AckServer(
+        pubSubConnection,
         serviceFactory.executorService(),
         serverFunction,
         descriptor.getServerTags().getInputTopic(),
