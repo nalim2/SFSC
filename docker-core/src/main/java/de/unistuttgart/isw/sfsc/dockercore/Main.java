2f27c039fca85fc442b5c665a2fd1043f73bbdfd
Matthias Milan Strljic <nalim2@users.noreply.github.com>
2021-01-29 12:36:09 +0100

    deploy-release-0.1.10 changed version number

--- docker-core/src/main/java/de/unistuttgart/isw/sfsc/dockercore/Main.java
+++ docker-core/src/main/java/de/unistuttgart/isw/sfsc/dockercore/Main.java
@@ -7,5 +7,6 @@ public class Main {
   public static void main(String[] args) throws Exception {
     System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "DEBUG");
     Core.start();
+
   }
 }
